b65d05ccc7d0e860b0a0f30f09a4fb42ce64234f
Gautam Kumar <gautam958@gmail.com>
2021-04-08 23:58:10 +0800

    Added react material css

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
-import React, { useEffect } from 'react';
-import LabourInputFormContainer from './Containers/Labour/LabourInputFormContainer';
-import ListTaskContainer from './Containers/Labour/ListTaskTypeContainer';
-import environment from './Environments/environment';
-
+import React, { useEffect } from "react";
+import LabourInputFormContainer from "./Containers/Labour/LabourInputFormContainer";
+import ListTaskContainer from "./Containers/Labour/ListTaskTypeContainer";
+import environment from "./Environments/environment";
+import { makeStyles } from "@material-ui/core/styles"; 
+import { AppBar, Toolbar, Grid } from "@material-ui/core";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
 function App(props) {
-
-  useEffect(() => {
-
-  }, []);
+  const classes = useStyles();
+  useEffect(() => {}, []);
 
   return (
     <div>
-
-      <h1 style={{ height: "90px", width: "150px", overflowx: "auto" }}>
-        <img src="./Logo.png" alt="logo" />
-        {environment.REACT_APP_appTitle}
-      </h1>
-
-      <div className="ui two column doubling stackable grid container">
-        <div className="column">
-          <LabourInputFormContainer />
-        </div>
-        <div className="column">
-          <ListTaskContainer />
-        </div>
+      <AppBar position="sticky">
+        <Toolbar>
+          <Grid container alignItems="center">
+            <Grid item>
+              <img
+                src="LabourReact/Logo.png"
+                alt="logo"
+                style={{ height: "50px", width: "300px" }}
+              />
+              <span>{environment.REACT_APP_appTitle}</span>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.root}>
+        <Grid container spacing={1}>
+          <Grid item xs={6}>
+            <LabourInputFormContainer />
+          </Grid>
+          <Grid item xs={6}>
+            <ListTaskContainer />
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
